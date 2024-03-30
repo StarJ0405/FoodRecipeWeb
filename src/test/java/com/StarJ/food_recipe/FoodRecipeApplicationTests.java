029aec81df5fd337f2335b2b69d4aef391289745
StarJ0405 <ghdtjdwo126@gmail.com>
2024-03-31 00:29:29 +0900

    basic

---
+++ src/test/java/com/StarJ/food_recipe/FoodRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StarJ.food_recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodRecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
