31cbee26fffc16202bca8fdd6f5d156507a5c33e
StarJ0405 <ghdtjdwo126@gmail.com>
2024-08-03 11:36:15 +0900

    darkmode 제거

--- src/main/resources/static/tailwind.config.js
+++ src/main/resources/static/tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
    content: ["../templates/**/*.html"],
+  darkMode: false,
   theme: {
     extend: {},
+  },
+  daisyui: {
+    themes: ["light"],
   },
   plugins: [
       require('daisyui')
